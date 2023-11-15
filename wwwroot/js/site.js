580b053263035c8473b1c3ad3089d48fdee0bd73
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-11-15 08:33:17 -0300

    Chau alerts

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,46 +1,4 @@
-﻿function checkLogin(log, Sesion = 0){
-    if (Sesion == 1){
-        switch (log){
-            case "-2":
-                console.log("Ese nobre de usuario ya existe :(");
-                Swal.fire({
-                    title: 'Error :(',
-                    text: 'El usuario ingresado ya existe',
-                    icon: 'warning',
-                });
-                break;
-            case "-1":
-                console.log("Ese usuario no existe :(");
-                Swal.fire({
-                    title: 'Error :(',
-                    text: 'El usuario ingresado no existe',
-                    icon: 'question',
-                });
-                break;
-            case "0":
-                console.log("Contraseña incorrecta :(");
-                Swal.fire({
-                    title: 'Error :(',
-                    text: 'La contraseña ingresada es incorrecta',
-                    icon: 'error',
-                });
-                break;
-            case "1":
-                console.log("Logeado correctamente :)");
-                Swal.fire({
-                    title: 'Perfe :)',
-                    text: 'Te has logeado correctamente',
-                    icon: 'success',
-                });
-                break;
-            default:
-                console.log("Default del switch - ViewBag.Log")
-                break;
-        }
-    }
-}
-
-function addFriend(){
+﻿function addFriend(){
     Swal.fire({
         title: "¿A quien le quiere enviar una solicitud de amistad?",
         input: "text",
